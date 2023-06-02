39343342faf29533f1d7cf2a3479318257cd9375
iasmimg <iasmimgaldino1401@gmail.com>
2023-06-02 16:59:23 -0300

    atualização 02.06

--- pg_inicial.php
+++ pg_inicial.php
@@ -1,106 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Quiz sobre Música</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-    <style type= "text/css">
-         .principal
-            {
-                background-color: pink;
-                border: 2px solid black; 
-                text-align: center;
-                color: black;
-                width: 500px;
-            }
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Quiz de Música</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<link rel="stylesheet" href="inicial.css">
+<script src='main.js'></script>
+   
+<body class = "principal">
+    <div >
+        <form action="">
+        <h1>
+            QUAL É A MÚSICA?
+        </h1>
+        <img src="qualmusica.png" class="img-fluid" alt="..." width="600px" > <br>
+
+        <p class= "nome"> 
+        <label for="nome">Digite seu nome: </label> 
+        <input type="text" name="nome" id="nome"/>
+         <button type="button" class="btn btn-primary ">Jogar</button>
+        </p>
+
+        
+        </form>
+    </div>
+<footer>
+<p> Trabalho para desciplina de Programação pra Internet <br>
+    Professor: Éberton Marinho <br>
+    Unidade 1
             
-   .rodape{
-    background-color: #f36e20;
-    color: white;
-    height: auto;
-    width: 100%;
-    font-size: 14px;
-    font-family: helvetica;
-    }
+</p>
+<article>
+<p class="desenvolv">Desenvolvedores <br>
+Alexandra Galvão - alexandra.galvao@escolar.ifrn.edu.br <br>
+Eliel Ewerton - eliel.ewerton@escolar.ifrn.edu.br <br>
+Iasmim Dias - iasmim.dias@escolar.ifrn.edu.br <br>
+João Victor Lima - victor.lima@escolar.ifrn.edu.br <br>
+Kaliny Gomes - kaliny.g@escolar.ifrn.edu.br</p>
 
-    .rodape a{
-    text-decoration: none;
-    color: white;
-    }
-    .rodape p{
-        margin-bottom: 5px;
-    }
-    .rodape-div{
-        display: flex;
-        flex-wrap: wrap;
-        width:100%;
-        margin: auto;
-        align-items: stretch;
-        padding: 60px 10px 40px 10px;
-    }
-    .rodape-div-1, .rodape-div-4{
-        display: flex;
-        width: calc(100% / 4 - 20px);
-        padding: 10px;
-    }
-    .rodape span{
-    font-size: 20px;
-    color: white;
-    }
-    .rodape-direitos{
-        width: calc(100% - 20px);
-        background-color: black;
-        padding: 10px;
-        margin: 0px;
-        text-align: center;
-    }
-    .rodape-div-2{
-        text-align = left;
-    }
-    </style>
-</head>
-<body>
-    <div class = "principal">
-    <form action="">
-    <h1>
-        QUAL É A MÚSICA?
-    </h1>
-    <p> 
-    <label for="nome">Digite seu nome: </label> 
-    <input type="text" name="nome" id="nome"/>
-    </p>
-    <footer class="rodape" id="contato">
-    <div class="rodape-div">
 
-        <div class="rodape-div-1">
-            <div class="rodape-div-1-coluna">
-                <p>Trabalho para a disciplina de Programação para Internet</p>
-                <p>Professor: Eberton Marinho</p>
-                <p>Unidade 1</p>
-            </div>
-        </div>
-
-    <div class="rodape-div-2">
-        <div class="rodape-div-3-coluna">
-            <p>Desenvolvedores:</p>
-            <p>Alexandra Galvão -<a href="#empresa"> alexandra.galvao@escolar.ifrn.edu.br</a></p>
-            <p>Eliel Ewerton -<a href="#sobre"> eliel.ewerton@escolar.ifrn.edu.br</a></p>
-            <p>Iasmim Dias -<a href="#sobre"> iasmim.dias@escolar.ifrn.edu.br</a></p>
-            <p>João Victor Lima -<a href="#sobre"> victor.lima@escolar.ifrn.edu.br</a></p>
-            <p>Kaliny Gomes -<a href="#sobre"> kaliny.g@escolar.ifrn.edu.br</a></p>
-        </div>
-    </div>
-
-    </div>
-    <p class="rodape-direitos">Copyright © 2023 – Todos os Direitos Reservados.</p>
 </footer>
-    
-    </form>
-    </div>
-    
-    
+
+   
 </body>
 </html>
