ad6bff37d84b3189ce49c096e7122da3d8fcbf19
iamimg <iamimgaldino1401@gmail.com>
2023-06-12 16:28:34 -0300

    atualizado

---
+++ Trap_1.php
@@ -0,0 +1,177 @@
+<?php
+    $resposta = 1;
+    $placar = 0;
+    $msg = '';
+    $ordemq = 0;
+
+    #Q1
+    $questao[0]['perguntas'] = "Quem canta essa música?";
+    $questao[0][0] = array("MC Cabelinho, Baco Exu do Blues e Delacruz", true);
+    $questao[0][1] = array("MC Cabelinho e Belo", false);
+    $questao[0][2] = array("KayBlack, Baco Exu do Blues e Nagalli", false);
+    $questao[0][3] = array("Veigh e Mc Luanna", false);
+
+    #Q2
+    $questao[1]['perguntas'] = "Que música é esta?";
+    $questao[1][0] = array("Drink", false);
+    $questao[1][1] = array("Luis V, Menina Linda", true);
+    $questao[1][2] = array("Quadro", false);
+    $questao[1][3] = array("Não Me Sinto Mal Mais", false);
+
+    #Q3
+    $questao[2]['perguntas'] = "Complete a música “Perdoa Por Tudo Vida”:";
+    $questao[2][0] = array("Só pra confirmar, ainda quer que eu deixe uma das minhas jóia", false);
+    $questao[2][1] = array("Perdoa por tudo, vida", false);
+    $questao[2][2] = array("Quem mandou te conhecer, malvada sem sentimento", false);
+    $questao[2][3] = array("Eu preciso ir, ela me diz: Mais tarde cê volta?", true);
+
+    #Q4
+    $questao[3]['perguntas'] = "Descubra a música (Cartão Black):";
+    $questao[3][0] = array("Flash", false);
+    $questao[3][1] = array("Meu Corre", false);
+    $questao[3][2] = array("Cartão Black", true);
+    $questao[3][3] = array("Probleminha", false);
+
+    #Q5
+    $questao[4]['perguntas'] = "Quem canta essa música? (Vestido da Fendi)";
+    $questao[4][0] = array("KayBlack", true);
+    $questao[4][1] = array("Oruam", false);
+    $questao[4][2] = array("WIU", false);
+    $questao[4][3] = array("BIN", false);
+
+    #Q6
+    $questao[5]['perguntas'] = "Complete a música “Ama o Jeito Que Me Odeia”:";
+    $questao[5][0] = array("Acostumado a nunca ser amado", false);
+    $questao[5][1] = array("Sempre soube que eu era um labirinto", true);
+    $questao[5][2] = array("Confio na sombra que segue meu corpo", false);
+    $questao[5][3] = array("Não tô perdido, cê se enganou", false);
+
+    #Q7
+    $questao[6]['perguntas'] = "Que música é essa?";
+    $questao[6][0] = array("Pandora", false);
+    $questao[6][1] = array("M4", false);
+    $questao[6][2] = array("Vampiro", true);
+    $questao[6][3] = array("Groupies", false);
+
+    #Q8
+    $questao[7]['perguntas'] = "Quem canta essa música (Quer Voar)?";
+    $questao[7][0] = array("KayBlack", false);
+    $questao[7][1] = array("Lezin", false);
+    $questao[7][2] = array("Sidoka", false);
+    $questao[7][3] = array("Matuê", true);
+
+    #Q9
+    $questao[8]['perguntas'] = "Complete a música “Conexões de Máfia”:";
+    $questao[8][0] = array("Ninguém nunca vai me atrasar", true);
+    $questao[8][1] = array("Coisa ruim vira ferida", false);
+    $questao[8][2] = array("Sem espaço pra quem vacilar", false);
+    $questao[8][3] = array("São conexões de máfia", false);
+
+    #Q10
+    $questao[9]['perguntas'] = "Quem canta essa música (Berço do Trap)?";
+    $questao[9][0] = array("Sidoka", false);
+    $questao[9][1] = array("Lezin", true);
+    $questao[9][2] = array("MC Caveirinha", false);
+    $questao[9][3] = array("Oruam", false);
+
+    if(isset($_POST['continuar'])){
+        $resposta=$_POST['continuar'];
+
+        if (isset($_POST["ordemq"])){
+            $ordemq = $_POST["ordemq"] + 1;
+        }
+
+        if (isset($_POST["questao"])){
+            $questao1=($_POST["questao"]);
+        }
+
+        $msg = " " . $placar + 10;
+        $resposta = "  " . $resposta;
+
+        if($questao1 == 1 ){
+            $msg = "Parabéns, Você Ganhou 10 Pontos!!";
+    
+        } else if(
+            $questao1 == 0){
+            $msg = "Que Pena, Você Errou :(";
+        }
+    }
+
+    function exibirQuestao($ordem){
+        global $questao;
+        global $msg;
+?>
+
+<h2><p><label><?php echo $questao[$ordem]['perguntas']?></label></p></h2>
+
+    <div class="box">
+        <img class="capa" src="trap.jpg" alt="">
+        <audio class="audio" src="link aqui" controls></audio></p>
+    </div>
+
+    <h3><input type="radio" name="questao" value="1"/><?php echo $questao[$ordem][0][0]; ?>  <br/>
+    <input type="radio" name="questao" value="0"/><?php echo $questao[$ordem][1][0]; ?> <br/>
+    <input type="radio" name="questao" value="0"/><?php echo $questao[$ordem][2][0]; ?> <br/>
+    <input type="radio" name="questao" value="0"/><?php echo $questao[$ordem][3][0]; ?> <br/></p></h3>
+
+<h3 class = "mensagem"><?php echo $msg?></h3></td>
+
+   <input type="submit" name="continuar" value="Continuar" class="btn btn-primary ">
+
+<?php
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>Trap</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<style>
+    .box{
+        height:50%;
+        width:70%;
+        display: flex;
+        align-items: center;
+        background: darkblue;
+        border-radius: 1px;
+        box-shadow: 0 .5px 1px black;
+        overflow: hidden;
+        margin:2px;
+        outline:none;
+    }
+
+    .audio{
+        height:70px;
+        width:80%;
+        display: flex;
+        outline:none;
+    }
+    .capa{
+        height:30%;
+        width:15%;
+    }
+    .musica{
+        font-family:Arial, Helvetica, sans-serif;
+        padding-top: 10px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+</style>
+<body class="musica">
+    <form method = "POST" action="Trap_1.php">
+    
+            <input type="text" name="ordemq" value="<?php echo $ordemq ?>"/>
+    
+
+            <?php exibirQuestao( $ordemq) ?>
+        
+    </form>
+</body>
+</html>
--- pg_inicial.php
+++ pg_inicial.php
@@ -7,8 +7,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <link rel="stylesheet" href="inicial.css">
 <script src='main.js'></script>
-   
-<body class = "principal">
+</head> 
+<body class = "principal" >
     <div >
         <form action="">
         <h1>
@@ -19,16 +19,18 @@
         <input type="text" name="nome" id="nome"/>
          <button type="button" class="btn btn-primary ">Jogar</button>
         </p>
+      
         
-        <label for="categoria">Escolha a Categoria:</label> <br>
         <div class="imgcategorias">
-        <img src="pop.jpg" class="img-thumbnail" alt="..." width="300px">
-        <img src="sertanejo.jpg" class="img-thumbnail" alt="..." width="300px">
-        <img src="trap.jpg" class="img-thumbnail" alt="..." width="300px">
+        <img src="poppb.jpg" class="img-thumbnail" alt="..." width="300px" a href="Trap_1.php">
+        <img src="sertanejopb.jpg" class="img-thumbnail" alt="..." width="300px">
+        <img src="trappb.jpg" class="img-thumbnail" alt="..." width="300px">
         </div>
         
         </form>
     </div>
+
+    
 <footer>
  <div class="footerleft">
  Trabalho para disciplina de Programação pra Internet <br>
